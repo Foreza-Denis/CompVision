7e9c061f9a70cecb00fea87fc38fe2bf2ed9ab3b
dolchik <dolchk763@gmail.com>
2021-10-23 19:03:53 +0300

    chore: конвертировали изображение в HSV в цикле
    чтобы более праильный цвет сделать

--- colorDetection.py
+++ colorDetection.py
@@ -7,6 +7,10 @@ h, w, _ = img.shape
 shrink_koeff = 2
 img = cv2.resize(img, (w//shrink_koeff, h//shrink_koeff), cv2.INTER_NEAREST)
 
+while True:
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-cv2.imshow("Orig", img)
-cv2.waitKey(0)
+    cv2.imshow("Orig", img)
+    cv2.imshow("HSV", imgHSV)
+    if cv2.waitKey(1) & 0xFF == ord('q'): # в орд можно поставить 27, это esc
+        break
